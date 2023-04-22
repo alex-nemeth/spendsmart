c0bd58b00ba29199a6f0ed2bb9ae09345076e58e
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-22 12:50:16 +0200

    budgetcard map loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
 import { useState } from "react";
 import BudgetCard from "./components/BudgetCard";
+import AddBudgetModal from "./components/AddBudgetModal";
+import { useBudgets } from "./contexts/BudgetsContext";
 
 function App() {
+    const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+    const { budgets, getBudgetExpenses } = useBudgets();
     return (
-        <div className="my-4">
-            <div className="flex gap-2 mb-4">
-                <h1 className="me-auto">SpendSmart</h1>
-                <button className="border-2">Add Budget</button>
-                <button className="border-2">Add Expense</button>
+        <>
+            <div className="my-4">
+                <div className="flex gap-2 mb-4">
+                    <h1 className="me-auto">SpendSmart</h1>
+                    <button
+                        className="border-2"
+                        onClick={() => setShowAddBudgetModal(true)}
+                    >
+                        Add Budget
+                    </button>
+                    <button className="border-2">Add Expense</button>
+                </div>
+                <div className="grid grid-cols-1 gap-1 items-start">
+                    {budgets.map((budget) => {
+                        const amount = getBudgetExpenses(budget.id).reduce(
+                            (total, expense) => total + expense.amount,
+                            0
+                        );
+                        return (
+                            <BudgetCard
+                                key={budget.id}
+                                name={budget.name}
+                                amount={amount}
+                                max={budget.max}
+                            />
+                        );
+                    })}
+                </div>
             </div>
-            <div className="grid grid-cols-1 gap-1 items-start">
-                <BudgetCard name="Entertainment" amount={200} max={300} />
-                <BudgetCard name="Gray" amount={100} max={300} gray />
-            </div>
-        </div>
+            <AddBudgetModal
+                show={showAddBudgetModal}
+                handleClose={() => setShowAddBudgetModal(false)}
+            />
+        </>
     );
 }
 
