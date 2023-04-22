3408cb8cb15d29686135790cfa04547121dd912f
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-22 12:46:58 +0200

    add budget modal component

---
+++ src/components/AddBudgetModal.jsx
@@ -0,0 +1,101 @@
+import React, { useRef } from "react";
+import { useBudgets } from "../contexts/BudgetsContext";
+
+export default function AddBudgetModal({ show, handleClose }) {
+    const nameRef = useRef();
+    const maxRef = useRef();
+    const { addBudget } = useBudgets();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        addBudget({
+            name: nameRef.current.value,
+            max: parseFloat(maxRef.current.value),
+        });
+        handleClose();
+    }
+
+    return (
+        <div
+            className={`${
+                show ? "visible" : "hidden"
+            } fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full`}
+        >
+            <div className="relative w-full max-w-md max-h-full">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <button
+                        type="button"
+                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                        data-modal-hide="authentication-modal"
+                        onClick={handleClose}
+                    >
+                        <svg
+                            aria-hidden="true"
+                            className="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                        <span className="sr-only">Close modal</span>
+                    </button>
+                    <div className="px-6 py-6 lg:px-8">
+                        <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                            New Budget
+                        </h3>
+                        <form className="space-y-6" onSubmit={handleSubmit}>
+                            <div>
+                                <label
+                                    for="name"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400"
+                                    placeholder="Entertainment"
+                                    ref={nameRef}
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    for="max"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Maximum Spending
+                                </label>
+                                <input
+                                    type="number"
+                                    name="max"
+                                    id="max"
+                                    placeholder="200"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    ref={maxRef}
+                                    required
+                                    step={0.01}
+                                />
+                            </div>
+                            <div className="flex justify-end">
+                                <button
+                                    type="submit"
+                                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                >
+                                    Add Budget
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
