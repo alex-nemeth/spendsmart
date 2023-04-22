30bfb9a259613e5400b1791b324096d42bcd0699
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-22 11:05:37 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
-    return <div>test</div>;
+    return (
+        <div className="my-4">
+            <div className="flex gap-2 mb-4">
+                <h1 className="me-auto">SpendSmart</h1>
+                <button className="border-2">Add Budget</button>
+                <button className="border-2">Add Expense</button>
+            </div>
+            <div className="grid grid-cols-1 gap-1 items-start"></div>
+        </div>
+    );
 }
 
 export default App;
