8d9c0e3847d46ed9f3d345acd28c7adaf426158e
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-04-22 15:26:16 +0200

    deleting budgets moves expenses to uncat

--- src/components/AddBudgetModal.jsx
+++ src/components/AddBudgetModal.jsx
@@ -39,7 +39,7 @@ export default function AddBudgetModal({ show, handleClose }) {
                             <path
                                 fill-rule="evenodd"
                                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
+                                clipRule="evenodd"
                             ></path>
                         </svg>
                         <span className="sr-only">Close modal</span>
@@ -51,7 +51,7 @@ export default function AddBudgetModal({ show, handleClose }) {
                         <form className="space-y-6" onSubmit={handleSubmit}>
                             <div>
                                 <label
-                                    for="name"
+                                    htmlFor="name"
                                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 >
                                     Name
@@ -68,7 +68,7 @@ export default function AddBudgetModal({ show, handleClose }) {
                             </div>
                             <div>
                                 <label
-                                    for="max"
+                                    htmlFor="max"
                                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 >
                                     Maximum Spending
--- src/contexts/BudgetsContext.jsx
+++ src/contexts/BudgetsContext.jsx
@@ -35,8 +35,11 @@ export const BudgetsProvider = ({ children }) => {
         });
     }
     function deleteBudget({ id }) {
-        setBudgets((prevBudgets) => {
-            return prevBudgets.filter((budget) => budget.id !== id);
+        setExpenses((prevExpenses) => {
+            return prevExpenses.map((expense) => {
+                if (expense.budgetId !== id) return expense;
+                return { ...expense, budgetId: UNCATEGORIZED_BUDGET_ID };
+            });
         });
     }
     function deleteExpense({ id }) {
