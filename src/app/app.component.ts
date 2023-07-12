ab93ebbbf23e98767cb0ecee93b5350f988fbec3
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-07-12 20:01:28 +0200

    login/logout, conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,32 @@ import { Component, OnInit, inject } from '@angular/core';
 import { BudgetsService } from './services/budgets.service';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   firestore: Firestore = inject(Firestore);
   budgets$: Observable<any[]>;
 
-  constructor() {
+  user!: firebase.User | null;
+
+  constructor(private auth: AngularFireAuth) {
     const collectionInstance = collection(this.firestore, 'budgets');
     this.budgets$ = collectionData(collectionInstance);
   }
 
-  addBudgetModal = true;
+  ngOnInit(): void {
+    this.auth.authState.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  addBudgetModal = false;
   addExpenseModal = false;
 
   toggleAddBudgetModal(): void {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,23 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  constructor(private auth: AngularFireAuth) {}
+
   @Output() addTrackerClick = new EventEmitter<void>();
 
   onAddTrackerClick() {
     this.addTrackerClick.emit();
+  }
+
+  logOut() {
+    this.auth
+      .signOut()
+      .then(() => console.log('Signed out'))
+      .catch((error) => console.error(error));
   }
 }
