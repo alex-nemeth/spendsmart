8b9dfedf6db09ae252967454c59ec336ce165d6a
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-07-13 23:27:31 +0200

    addExpense successful

--- src/app/add-expense-modal/add-expense-modal.component.ts
+++ src/app/add-expense-modal/add-expense-modal.component.ts
@@ -1,7 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BudgetsService } from '../services/budgets.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Firestore } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  updateDoc,
+  doc,
+  arrayUnion,
+} from '@angular/fire/firestore';
+import { IExpense } from 'src/shared/interfaces';
 
 @Component({
   selector: 'app-add-expense-modal',
@@ -24,14 +31,25 @@ export class AddExpenseModalComponent {
   }
 
   handleSubmit(f: any) {
+    console.log(f.value);
     this.auth.authState.subscribe((user) => {
       if (user) {
         const uid = user.uid;
-        // const collectionInstance = collection(this.firestore, `users/${uid}/budgets/`)
+        const docInstance = doc(
+          this.firestore,
+          `users/${uid}/budgets/${this.id}`
+        );
+        console.log('UserID: ' + uid + 'Budget ID: ' + this.id + f.value);
+        const newExpense = {
+          ...f.value,
+        };
+        updateDoc(docInstance, {
+          expenses: arrayUnion(newExpense),
+        })
+          .then(() => console.log('Expense Added!'))
+          .catch((error) => console.error(error));
       }
     });
-
-    // this.budgetsService.addExpense(this.description, this.amount, this.id);
     this.handleClose();
   }
 }
