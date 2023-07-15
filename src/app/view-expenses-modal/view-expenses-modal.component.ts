c05ed7a55a96c5c5decb81c5f64cd4ad10c0f189
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-07-15 11:58:11 +0200

    deleting expenses functional, not rerendering

--- src/app/view-expenses-modal/view-expenses-modal.component.ts
+++ src/app/view-expenses-modal/view-expenses-modal.component.ts
@@ -1,14 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { IBudget } from 'src/shared/interfaces';
+import { IBudget, IExpense } from 'src/shared/interfaces';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Firestore, doc, deleteDoc } from '@angular/fire/firestore';
+import { arrayRemove, updateDoc } from 'firebase/firestore';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-view-expenses-modal',
   templateUrl: './view-expenses-modal.component.html',
 })
 export class ViewExpensesModalComponent {
-  constructor(private auth: AngularFireAuth, private firestore: Firestore) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: Firestore,
+    private changeDetector: ChangeDetectorRef
+  ) {}
 
   @Input() id: string = '';
   @Input() budget: IBudget = {
@@ -34,6 +40,25 @@ export class ViewExpensesModalComponent {
           `users/${user.uid}/budgets/${this.id}`
         );
         deleteDoc(docInstance).then(() => console.log('Budget deleted'));
+      }
+    });
+  }
+
+  deleteExpense(expense: IExpense) {
+    const selectedBudgetId = this.budget.id;
+    const selectedExpense = expense;
+    this.auth.authState.subscribe((user) => {
+      if (user) {
+        const docInstance = doc(
+          this.firestore,
+          `users/${user.uid}/budgets/${selectedBudgetId}`
+        );
+        updateDoc(docInstance, {
+          expenses: arrayRemove(selectedExpense),
+        }).then(() => {
+          console.log('Expense deleted!');
+          this.changeDetector.detectChanges();
+        });
       }
     });
   }
