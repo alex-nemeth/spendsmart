a294e113e4228d75d6d684a0e4682f8c58787506
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-07-27 21:08:33 +0200

    components folder refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BudgetCardComponent } from './budget-card/budget-card.component';
 import { BudgetsService } from './shared/services/budgets.service';
-import { AddBudgetModalComponent } from './add-budget-modal/add-budget-modal.component';
+import { AddBudgetModalComponent } from './components/add-budget-modal/add-budget-modal.component';
 import { FormsModule } from '@angular/forms';
-import { AddExpenseModalComponent } from './add-expense-modal/add-expense-modal.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ViewExpensesModalComponent } from './view-expenses-modal/view-expenses-modal.component';
-import { TotalBudgetCardComponent } from './total-budget-card/total-budget-card.component';
 import { DateToLocalePipe } from './shared/pipes/date-to-locale.pipe';
-import { RegistrationComponent } from './registration/registration.component';
-import { StartupComponent } from './startup/startup.component';
+
+import { HeaderComponent } from './components/header/header.component';
+import { BudgetCardComponent } from './components/budget-card/budget-card.component';
+import { AddExpenseModalComponent } from './components/add-expense-modal/add-expense-modal.component';
+import { LoginComponent } from './components/login/login.component';
+import { ViewExpensesModalComponent } from './components/view-expenses-modal/view-expenses-modal.component';
+import { TotalBudgetCardComponent } from './components/total-budget-card/total-budget-card.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { StartupComponent } from './components/startup/startup.component';
 
 @NgModule({
   declarations: [
